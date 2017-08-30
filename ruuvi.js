b1b18caf4c2ce7b813f4f58a31a276d7008e9d6e
Brian Lalor <blalor@bravo5.org>
2017-08-30 07:16:59 -0400

    initial commit

---
+++ ruuvi.js
@@ -0,0 +1,96 @@
+"use strict";
+
+const YAML = require("js-yaml");
+const FS = require("fs");
+const Ruuvi = require("node-ruuvitag");
+const Influx = require("influx");
+
+var config = YAML.safeLoad(FS.readFileSync("config.yml", "utf-8"));
+
+const influx = new Influx.InfluxDB({
+    host: config.influx.host,
+    database: config.influx.database,
+    schema: [
+        {
+            measurement: "meta",
+            fields: {
+                rssi: Influx.FieldType.INTEGER,
+                battery: Influx.FieldType.INTEGER,
+            },
+            tags: [
+                "id",
+                "label",
+            ],
+        },
+        {
+            measurement: "accel",
+            fields: {
+                x: Influx.FieldType.INTEGER,
+                y: Influx.FieldType.INTEGER,
+                z: Influx.FieldType.INTEGER,
+            },
+            tags: [
+                "id",
+                "label",
+            ],
+        },
+        {
+            measurement: "atmosphere",
+            fields: {
+                humidity: Influx.FieldType.FLOAT,
+                temperature: Influx.FieldType.FLOAT,
+                pressure: Influx.FieldType.INTEGER,
+            },
+            tags: [
+                "id",
+                "label",
+            ],
+        },
+    ]
+})
+
+
+Ruuvi.on("found", function(tag) {
+    console.log("found " + tag.id);
+    
+    tag.on("updated", function(data) {
+        console.log("Got data from RuuviTag " + tag.id + ":\n" + JSON.stringify(data, null, "\t"));
+        
+        var tags = {
+            id: tag.id,
+        };
+        
+        if (config.devices[tag.id]) {
+            tags["label"] = config.devices[tag.id].label;
+        }
+        
+        influx.writePoints([
+            {
+                measurement: "meta",
+                tags: tags,
+                fields: {
+                    rssi: data.rssi,
+                    battery: data.battery,
+                },
+            },
+            {
+                measurement: "accel",
+                tags: tags,
+                fields: {
+                    x: data.accelerationX,
+                    y: data.accelerationY,
+                    z: data.accelerationZ,
+                },
+            },
+            {
+                measurement: "atmosphere",
+                tags: tags,
+                fields: {
+                    humidity: data.humidity,
+                    temperature: data.temperature,
+                    pressure: data.pressure,
+                },
+            },
+        ]);
+    });
+});
